1a7bedab49f582fa08d5a2854c470de32e088265
Aris <akoumis1@gmail.com>
2016-04-23 12:30:17 -0700

    passed events from EONET to twitter API

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ var options = {
   path: '/api/v2.1/events'
 };
 
+var nasaData = {};
+
 callback = function(response) {
   var data;
   var str = "";
+
 
   response.on('data', function (chunk) {
     str += chunk.toString();
@@ -19,7 +22,7 @@ callback = function(response) {
   response.on('end', function () {
     data = JSON.parse(str)
     data.events.forEach(function(event) {
-        console.log(event.title)
+        getTweets(event)
     })
   });
 }
@@ -28,15 +31,26 @@ http.request(options, callback).end();
 
 //Twitter API
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: '***REMOVED***',
+  consumer_secret: '***REMOVED***',
+  access_token_key: '***REMOVED***',
+  access_token_secret: '***REMOVED***'
 });
  
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
-    console.log(tweets);
-  }
+function getTweets(event) {
+  client.get('search/tweets', {q: event.title}, function(error, tweets, response){
+    tweets.statuses.forEach(function(tweet) {
+      console.log(tweet)
+    })
+  });
+}
+
+client.stream('statuses/filter', {track: 'yungkoum'},  function(stream){
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+  stream.on('error', function(error) {
+    console.log(error);
+  });
 });
